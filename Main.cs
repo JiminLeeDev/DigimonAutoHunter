6e8221160660d2e1bdb1f617394395ee352e884a
jiminleedev0220 <jiminleedev0220@gmail.com>
2022-03-31 23:57:42 +0900

    초기 커밋.

---
+++ Main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace DigimonAutoHunter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+		}
+	}
+}
